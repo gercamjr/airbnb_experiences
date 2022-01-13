4d867ff4ae791bc38118a3fc5d5c60bddc396c80
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-13 09:21:13 -0600

    created card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,25 @@
-import React from "react"
-
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
+import React from "react";
+import cardimg from "../images/katie-zaferes.png";
+import cardstar from "../images/star.png";
 
 export default function Card() {
-    return ( 
-        <h1> Replace this </h1>
-    )
+  return (
+    <section className="card">
+    <p className="card__status">SOLD OUT</p>
+      <img
+        src={cardimg}
+        alt="image of katie zaferes in swimming competition"
+        className="card__image"
+      />
+      
+      <div className="card__stats">
+        <img src={cardstar} alt="red star" className="card__star" />
+        <p className="card__rating">5.0&nbsp; </p>
+        <p className="card__review-count">&nbsp;(6) USA</p>
+      </div>
+
+      <p className="card__title">Life lessons with Katie Zaferes</p>
+      <p className="card__cost"><strong>From $136 </strong> / person</p>
+    </section>
+  );
 }
