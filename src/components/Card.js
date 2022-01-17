7076b608122adbc2f347ad1e72d0e074fd149f21
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-01-17 11:48:28 -0600

    updated readme

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import cardimg from "../images/katie-zaferes.png";
 import cardstar from "../images/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <section className="card">
     <p className="card__status">SOLD OUT</p>
@@ -14,13 +14,13 @@ export default function Card() {
       
       <div className="card__stats">
         <img src={cardstar} alt="red star" className="card__star" />
-        <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+        <span>{props.rating}</span>
+                <span className="gray">{props.count}</span>
+                <span className="gray">{props.country}</span>
       </div>
 
-      <p className="card__title">Life lessons with Katie Zaferes</p>
-      <p className="card__cost"><span className="bold">From $136 </span> / person</p>
+      <p className="card__title">{props.title}</p>
+      <p className="card__cost"><span className="bold">From $136{props.price} </span> / person</p>
     </section>
   );
 }
